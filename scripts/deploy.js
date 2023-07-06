6a0c43ddbae56d044c878016bfaffe23d46f191e
cyferdev <mrnzero321@gmail.com>
2023-07-06 12:53:51 +0530

    deployed

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+// Import the Hardhat package
+const hre = require("hardhat");
+
+async function main() {
+  const nftContract = await hre.ethers.deployContract("NFTee");
+  await nftContract.waitForDeployment();
+  console.log("the address is : ",nftContract.target)
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
